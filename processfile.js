f9297465a393497cd9b2302132afed87b70b043b
Spencer Najib <spencernajib2@gmail.com>
2022-10-17 13:04:04 +0100

    added support for both hardware acceleration with cuda and standard CPU

--- processfile.js
+++ processfile.js
@@ -2,15 +2,19 @@ import ffmpeg from "fluent-ffmpeg"
 import path from "path"
 
 const isWindows = process.platform.includes("win")
-async function Process(filename, dir, size, threadId = "single") {
+async function Process(filename, dir, size, threadId = "single",hardwareAccel=false) {
 	console.time(`${threadId}`)
 	//process  the compression and size formating;
 	if (isWindows) {
 		ffmpeg.setFfmpegPath("C:\\ffmpeg\\bin\\ffmpeg.exe")
 	}
 	 //console.log('file',filename)
-
-	ffmpeg({ source: filename, })
+	 /*  HARWARE ACCELERATION
+	 * using cuda
+	 * recompile ffmpeg with hardwareAccel support (https://docs.nvidia.com/video-technologies/video-codec-sdk/ffmpeg-with-nvidia-gpu/)
+	 */
+ if (hardwareAccel) {
+		ffmpeg({ source: filename, })
 		.addInputOptions(["-hwaccel cuda","-r 30"])
 
 		//h264
@@ -28,7 +32,28 @@ async function Process(filename, dir, size, threadId = "single") {
 		})
 
 		.save(`./${dir}/${Date.now()}_${size}P_${path.join(filename)}`)
+	}
+	else { 
+		 ffmpeg({ source: filename, logger: console })
+		.fps(30)
 
+		// h264
+		.videoCodec("libx264")
+		.audioCodec("libmp3lame")
+		.addOptions(["-crf 28"])
+		.size(`?x${size}`) //, HD, FHD, SD dimensions
+		.on("end", (result) => {
+			console.log(result)
+			console.timeEnd(`${threadId}`)
+		})
+		.on("error", (err) => {
+			console.log(err)
+		})
+
+		.save(`./${dir}/${Date.now()}_${size}P_${path.join(filename)}`)
+		return file;
+		//console.log(file)
+	}
 	//*/
 }
 
