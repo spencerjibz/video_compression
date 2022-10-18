6f9fd04e85d35ef50d351e3fe18b1490a74f78d3
sdotpenny <sdotpenny@sdot.local>
2022-10-18 22:17:35 +0100

    add unified function

---
+++ compressVideo.js
@@ -0,0 +1,49 @@
+import { Worker, isMainThread } from "node:worker_threads"
+import path, { dirname } from "node:path"
+import compressFile from "./processfile.js"
+async function ProcessFile(filename, dir, sizes, multithreaded = true,hwaccel = false) {
+	if (multithreaded) {
+		console.log("------------Multi-threaded Process --------------------")
+		/* MULTITHREADED WORK -> n threads for n sizes*/
+		let files = await Promise.all(
+			sizes.map((size) => runinParallel(filename, dir, size,hwaccel))
+		)
+		return files
+	} else {
+		/*SINGLE THREADED-> n task size for n sizes*/
+		console.log("-------------SINGLE THREADED--------------")
+		//
+		//console.time("singlethreaded")
+		let result = await Promise.all(
+			sizes.map((size) =>
+				compressFile(filename, dir, size, "task" + size,true)
+			)
+		);
+        return result;
+	}
+}
+
+function runinParallel(filename, dir, size,hwaccel) {
+	return new Promise(function (resolve, reject) {
+		const worker = new Worker("./worker.js", {
+			workerData: {
+				filename,
+				dir: path.join("./" + dir),
+				size,
+                hwaccel
+			},
+		})
+		// handle messages from worker_threads
+
+		worker.on("message", (msg) => {
+			resolve(msg)
+		})
+		worker.on("error", (err) => {
+			reject(err)
+		})
+	})
+}
+export default ProcessFile
+
+let res = await ProcessFile('sample4k.mp4','compressed',[1080,720,480,360],true,true)
+console.log(res)
